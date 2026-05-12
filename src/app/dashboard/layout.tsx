e2c0d85c86c9249275ea569d0d2532778f41a34f
MaxDM <maxdiazm.2004@gmail.com>
2026-05-11 21:13:56 -0600

    feat: dashboard page implemented, added charts components (from MUI lib)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
-import Navbar from '@/components/shared/Navbar';
-import ProtectedRoute from '@/components/features/auth/ProtectedRoute';
+import Navbar from "@/components/shared/Navbar";
+import ProtectedRoute from "@/components/features/auth/ProtectedRoute";
 
 export default function DashboardLayout({
   children,
@@ -7,16 +7,12 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ProtectedRoute
-      allowedRoles={['Admin', 'Maestro']}
-    >
+    <ProtectedRoute allowedRoles={["Admin", "Maestro"]}>
       <div className="min-h-screen bg-neutral-100 flex flex-col">
         <Navbar />
 
         <main className="flex-1 p-4 sm:p-6 lg:p-8">
-          <div className="max-w-3xl mx-auto w-full">
-            {children}
-          </div>
+          <div className="max-w-screen mx-auto w-full">{children}</div>
         </main>
       </div>
     </ProtectedRoute>
